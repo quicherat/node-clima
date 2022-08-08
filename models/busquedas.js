1495d1cdcd04189bf7971f7cb9407179d63603c4
quicherat <enriquevillasolis@gmail.com>
2022-08-08 11:29:55 -0500

    Se creó instancias de Axios

--- models/busquedas.js
+++ models/busquedas.js
@@ -8,12 +8,24 @@ export class Busquedas{
         //TODO leer DB si existe
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoicXVpY2hlcmF0IiwiYSI6ImNsNmp1M2h2NjIxamsza3BzY2VuMmp4cmYifQ.8c9p1BbfWFfe-_LmMOokNw',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     async ciudad( lugar = ''){
 
         try {
             // petición http
-            // console.log('ciudad', lugar);
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/vancouver.json?proximity=ip&language=es&access_token=pk.eyJ1IjoicXVpY2hlcmF0IiwiYSI6ImNsNmp1M2h2NjIxamsza3BzY2VuMmp4cmYifQ.8c9p1BbfWFfe-_LmMOokNw');
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            });
+
+            const resp = await instance.get();           
             console.log(resp.data);
     
             return []; //retorna las ciudades
